dc1fcd0bcad948b2099dab33bba6f21b17c7dd15
Alex Sandri <me@alexsandri.com>
2020-09-21 16:11:15 +0200

    Fix: getter docs called on null

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class MyApp extends StatelessWidget {
         body: StreamBuilder<QuerySnapshot>(
           stream: db.collection("circolari").snapshots(),
           builder: (context, snapshot) {
-            if (snapshot.data.docs.isEmpty)
+            if (!snapshot.hasData || snapshot.data.docs.isEmpty)
               return Center(
                 child: Text("Non sono presenti circolari"),
               );
