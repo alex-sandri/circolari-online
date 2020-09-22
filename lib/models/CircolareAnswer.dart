5a25517c52a39d38f11f51794eeb7ab363fd8c5f
Alex Sandri <me@alexsandri.com>
2020-09-22 17:27:36 +0200

    Add CircolareAnswer.parentId field

--- lib/models/CircolareAnswer.dart
+++ lib/models/CircolareAnswer.dart
@@ -5,16 +5,21 @@ class CircolareAnswer
 {
   final String id;
 
+  /// The `id` of the `Circolare` this answer belongs to
+  final String parentId;
+
   final Map<String, dynamic> fields;
 
   CircolareAnswer({
     @required this.id,
+    @required this.parentId,
     @required this.fields,
   });
 
   static CircolareAnswer fromFirestore(DocumentSnapshot document) =>
     CircolareAnswer(
       id: document.id,
+      parentId: document.reference.parent.parent.id,
       fields: document.data(),
     );
 }
