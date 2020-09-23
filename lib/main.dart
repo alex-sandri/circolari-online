5e3137b32dbed0763a2e2dfa0ec53275aa49357d
Alex Sandri <me@alexsandri.com>
2020-09-23 19:34:49 +0200

    Remove unused var

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:circolari_online/routes/Home.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
@@ -13,8 +12,6 @@ void main() async {
 }
 
 class MyApp extends StatelessWidget {
-  final FirebaseFirestore db = FirebaseFirestore.instance;
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
