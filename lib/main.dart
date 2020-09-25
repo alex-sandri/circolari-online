99d588869d2970b19aa46b9ea7b8d313ea34b71a
Alex Sandri <me@alexsandri.com>
2020-09-25 14:58:13 +0200

    Remove useless code

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:circolari_online/routes/Home.dart';
-import 'package:circolari_online/routes/SignIn.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
@@ -20,10 +18,7 @@ class MyApp extends StatelessWidget {
       title: "Circolari Online",
       theme: ThemeData.light(),
       darkTheme: ThemeData.dark(),
-      home: StreamBuilder<User>(
-        stream: FirebaseAuth.instance.authStateChanges(),
-        builder: (context, snapshot) => snapshot.data == null ? Home() : Home(),
-      ),
+      home: Home(),
       debugShowCheckedModeBanner: false,
       localizationsDelegates: [
         GlobalMaterialLocalizations.delegate,
