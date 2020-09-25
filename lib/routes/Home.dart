574de1b7346f05656be3d51a4d64418c80d20311
Alex Sandri <me@alexsandri.com>
2020-09-25 16:15:47 +0200

    Refresh home on sign in / sign out

--- lib/routes/Home.dart
+++ lib/routes/Home.dart
@@ -18,108 +18,113 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text("Circolari Online"),
-          actions: [
-            IconButton(
-              icon: Icon(Icons.qr_code),
-              tooltip: "Compila circolare",
-              onPressed: () async {
-                final ScanResult result = await BarcodeScanner.scan(
-                  options: ScanOptions(
-                    restrictFormat: [ BarcodeFormat.qr ],
-                    strings: {
-                      "cancel": "Annulla",
-                      "flash_on": "Accendi flash",
-                      "flash_off": "Spegni flash",
-                    },
-                  ),
-                );
-
-                if (result.type == ResultType.Barcode)
-                {
-                  final String id = result.rawContent;
-
-                  final Circolare circolare = await Circolare.get(id);
-
-                  Navigator
-                    .of(context)
-                    .push(MaterialPageRoute(
-                      builder: (context) => CircolarePage(circolare),
-                    ));
-                }
-              },
-            ),
-
-            if (_isSignedIn())
-              IconButton(
-                icon: Icon(Icons.exit_to_app),
-                tooltip: "Esci",
-                onPressed: FirebaseAuth.instance.signOut,
-              ),
-
-            if (!_isSignedIn())
-              IconButton(
-                icon: Icon(Icons.login),
-                tooltip: "Accedi",
-                onPressed: () => Navigator.of(context).push(MaterialPageRoute(builder: (context) => SignIn())),
-              ),
-          ],
-        ),
-        body: _isSignedIn()
-          ?
-            StreamBuilder<List<Circolare>>(
-              stream: Circolare.getAll(),
-              builder: (context, snapshot) {
-                if (!snapshot.hasData) return LinearProgressIndicator();
-
-                if (snapshot.data.isEmpty)
-                  return Center(
-                    child: Text("Non sono presenti circolari"),
-                  );
-
-                return ListView.separated(
-                  separatorBuilder: (context, index) => Divider(),
-                  itemCount: snapshot.data.length,
-                  itemBuilder: (context, index) {
-                    final Circolare circolare = snapshot.data[index];
-
-                    return ListTile(
-                      title: Text(circolare.title),
-                      onTap: () => Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => CircolareAnswersPage(circolare)
-                        ),
+      child: StreamBuilder<User>(
+        stream: auth.authStateChanges(),
+        builder: (context, snapshot) {
+          return Scaffold(
+            appBar: AppBar(
+              title: Text("Circolari Online"),
+              actions: [
+                IconButton(
+                  icon: Icon(Icons.qr_code),
+                  tooltip: "Compila circolare",
+                  onPressed: () async {
+                    final ScanResult result = await BarcodeScanner.scan(
+                      options: ScanOptions(
+                        restrictFormat: [ BarcodeFormat.qr ],
+                        strings: {
+                          "cancel": "Annulla",
+                          "flash_on": "Accendi flash",
+                          "flash_off": "Spegni flash",
+                        },
                       ),
                     );
+
+                    if (result.type == ResultType.Barcode)
+                    {
+                      final String id = result.rawContent;
+
+                      final Circolare circolare = await Circolare.get(id);
+
+                      Navigator
+                        .of(context)
+                        .push(MaterialPageRoute(
+                          builder: (context) => CircolarePage(circolare),
+                        ));
+                    }
                   },
-                );
-              }
-            )
-          :
-            Container(
-              width: double.infinity,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text("Accedi per creare circolari"),
-                  FlatButton.icon(
+                ),
+
+                if (_isSignedIn())
+                  IconButton(
+                    icon: Icon(Icons.exit_to_app),
+                    tooltip: "Esci",
+                    onPressed: FirebaseAuth.instance.signOut,
+                  ),
+
+                if (!_isSignedIn())
+                  IconButton(
                     icon: Icon(Icons.login),
-                    label: Text("Accedi"),
+                    tooltip: "Accedi",
                     onPressed: () => Navigator.of(context).push(MaterialPageRoute(builder: (context) => SignIn())),
                   ),
-                ],
+              ],
+            ),
+            body: _isSignedIn()
+              ?
+                StreamBuilder<List<Circolare>>(
+                  stream: Circolare.getAll(),
+                  builder: (context, snapshot) {
+                    if (!snapshot.hasData) return LinearProgressIndicator();
+
+                    if (snapshot.data.isEmpty)
+                      return Center(
+                        child: Text("Non sono presenti circolari"),
+                      );
+
+                    return ListView.separated(
+                      separatorBuilder: (context, index) => Divider(),
+                      itemCount: snapshot.data.length,
+                      itemBuilder: (context, index) {
+                        final Circolare circolare = snapshot.data[index];
+
+                        return ListTile(
+                          title: Text(circolare.title),
+                          onTap: () => Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => CircolareAnswersPage(circolare)
+                            ),
+                          ),
+                        );
+                      },
+                    );
+                  }
+                )
+              :
+                Container(
+                  width: double.infinity,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("Accedi per creare circolari"),
+                      FlatButton.icon(
+                        icon: Icon(Icons.login),
+                        label: Text("Accedi"),
+                        onPressed: () => Navigator.of(context).push(MaterialPageRoute(builder: (context) => SignIn())),
+                      ),
+                    ],
+                  ),
+                ),
+            floatingActionButton: !_isSignedIn() ? null : Builder(
+              builder: (context) => FloatingActionButton(
+                child: Icon(Icons.add),
+                tooltip: "Crea nuova circolare",
+                onPressed: () => Navigator.of(context).push(MaterialPageRoute(builder: (context) => CreateCircolarePage())),
               ),
             ),
-        floatingActionButton: !_isSignedIn() ? null : Builder(
-          builder: (context) => FloatingActionButton(
-            child: Icon(Icons.add),
-            tooltip: "Crea nuova circolare",
-            onPressed: () => Navigator.of(context).push(MaterialPageRoute(builder: (context) => CreateCircolarePage())),
-          ),
-        ),
+          );
+        }
       ),
     );
   }
